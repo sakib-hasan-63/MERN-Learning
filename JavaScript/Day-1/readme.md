6f01abd269af108526732dabd20a3c9a9990dac5
Sakib Hasan <sakibj137@gmail.com>
2026-07-22 02:23:35 +0530

    Day 01 - Variables completed

---
+++ JavaScript/Day-1/readme.md
@@ -0,0 +1,14 @@
+# Day 01 - Variables
+
+Topic:
+Variables
+
+Learned:
+- let
+- const
+- var
+
+Practice:
+Student Information
+
+Completed
---
+++ JavaScript/Day-1/script.js
@@ -0,0 +1,8 @@
+var name = "sakib";
+console.log(name);
+
+let age = 20;
+console.log(age);
+
+const address = "Lucknow";
+console.log(address);
