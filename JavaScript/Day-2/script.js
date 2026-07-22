f114acedc54a035824863a63ed969dcb77f22bcd
Sakib Hasan <sakibj137@gmail.com>
2026-07-22 20:00:21 +0530

    Day-2-Data Types Completed

---
+++ JavaScript/Day-2/script.js
@@ -0,0 +1,20 @@
+//Data Types
+//There are two types of data
+// 1.Primitive
+// 2.Non-Primitive
+
+//Primitive 
+
+let age = "20";
+let name = "Sakib";
+let isLoggedIn = true;
+let score;
+let skills = null;
+let hugeNumber = 126373773733838n;
+
+console.log(age);
+console.log(name);
+console.log(isLoggedIn);
+console.log(score);
+console.log(skills);
+console.log(hugeNumber);
