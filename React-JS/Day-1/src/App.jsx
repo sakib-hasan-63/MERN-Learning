ab69b7dc1013e374e5f7560b2d8290ff80f4d15a
Sakib Hasan <sakibj137@gmail.com>
2026-07-24 21:54:50 +0530

    Day 1: React setup and basic component

---
+++ React-JS/Day-1/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+     <h1>React Day-1</h1>
+     <p>Here we have leran to how to setup the react project abd how it is works.</p>
+     </>
+  )
+}
+
+export default App
