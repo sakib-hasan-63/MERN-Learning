c01e58de0f602050164a233f61098ec9c6df22f0
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-08 15:06:49 +0530

    Day 11- Functions Completed

---
+++ JavaScript/Day-11/script2.js
@@ -0,0 +1,13 @@
+// Arrow function is a shorter and cleaner syntax for writing functions without using the function keyword
+
+const greet = (name) =>{
+    console.log(`hello ${name}`);
+}
+
+greet("sakib");
+
+// One line function 
+
+const add = (a,b) => a+b;
+
+console.log(add(10,20));
