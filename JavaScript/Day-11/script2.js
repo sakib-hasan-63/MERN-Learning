4cb7d7f67f1dd56eab7ca32b50222ff0cb9caaab
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-09 20:59:46 +0530

    Day 11- Functions Completed

--- JavaScript/Day-11/script2.js
+++ JavaScript/Day-11/script2.js
@@ -11,3 +11,24 @@ greet("sakib");
 const add = (a,b) => a+b;
 
 console.log(add(10,20));
+
+//Hoisting
+hello();
+function hello(){
+    console.log("hello world");
+
+// it is work only function initlization not work with arrow fun and function expresion
+}
+
+// Function inside the function
+
+function mainFun(){
+    let name = "sakib";
+    const showName = () =>{
+        console.log(`Name is : ${name}`);
+        const add = (a,b)=> a+b;
+    }
+    console.log(add(20,30));
+    showName();
+}
+mainFun();
