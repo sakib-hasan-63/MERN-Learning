90881adf57b4fd6e8ec58d2dc31fecbf39706751
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-15 03:17:31 +0530

    Day 6- Completed

---
+++ React-JS/Password-Generator/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+      <h1 className='text-3xl font-bold text-white text-center mb-4'>
+        Password Generator
+      </h1>
+  )
+}
+
+export default App
