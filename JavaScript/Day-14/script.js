3c8658e415693b5bacf1412f0f6024f6f4f4a663
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-08-18 02:22:24 +0530

    Day -14 Sort and Find Method Completed

--- JavaScript/Day-14/script.js
+++ JavaScript/Day-14/script.js
@@ -1,1 +1,47 @@
-console.log(hello);
+// Sort method
+
+// besically it proper work at string
+// and in the case of number we have to pass callback function then it is work asc and dsc
+// sort method change the array 
+// if we want to dont change our original array then we use to slice() method
+
+let str = ["sakib","ayan","vali","rehan","tahseen"];
+ const ans = str.sort();
+console.log(ans)
+
+// in case of number 
+//Assending order
+let num = [12,33,4,55,3444,55,56,6666];
+const ans2 = num.sort((a,b)=> a-b);
+console.log(ans2)
+
+// Desending Order
+
+let num2 = [12,33,4,55,3444,55,56,6666];
+const ans3 = num2.sort((a,b)=> b-a);
+console.log(ans3)
+
+// Find Method
+
+const myarr = ["cat","dog","lion","elephent","monkey"];
+
+function isLength4(str){
+    return (str.length === 4);
+}
+
+const myans = myarr.find(isLength4)
+const myans2 = myarr.find((str)=> str.length === 6); // it is also do same things in one line
+console.log(myans)
+console.log(myans2)
+
+// Real World use case
+
+const users =[
+    {userId:1 , name:"sakib"},
+    {userId:2 , name:"kaif"},
+    {userId:3 , name:"saif"},
+    {userId:4 , name:"Sakib Hasan"}
+];
+
+const findUser4 = users.find((user)=> user.userId === 4);
+console.log(findUser4);
