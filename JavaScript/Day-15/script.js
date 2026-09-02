415c41824a026dc71c458317c13ea1c4985d63bb
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-09-02 23:30:39 +0530

    Splice Method Completed

--- JavaScript/Day-15/script.js
+++ JavaScript/Day-15/script.js
@@ -18,3 +18,12 @@ arr1.splice(5,0,60);
 
 console.log(arr1);
 
+// Delete And Insert
+
+const arr = [1,2,3,4,5];
+
+const deletee = arr.splice(0,2,8,9,10);
+
+console.log(deletee)
+
+console.log(arr);
