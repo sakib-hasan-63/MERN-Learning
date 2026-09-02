c767585dcb54312cf34f2aab0daeaf670323734a
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-09-02 23:23:04 +0530

    Splice Method Insert Item Completed

--- JavaScript/Day-15/script.js
+++ JavaScript/Day-15/script.js
@@ -1,4 +1,11 @@
-function day(num){
-    console.log(`Day -${num}`);
-}
-day(15);
+// Splice Method
+
+const myarr = ["item1", "item2", "item3"];
+
+myarr.splice(1,1);
+
+const deleteItem = myarr.splice(1,2);
+
+console.log(`Deleted item ${deleteItem}`);
+
+console.log(myarr)
