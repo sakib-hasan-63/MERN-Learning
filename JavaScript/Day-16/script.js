649834ad4ddaa55216b550687f418fc358913342
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-09-03 23:47:08 +0530

    Set Completed

--- JavaScript/Day-16/script.js
+++ JavaScript/Day-16/script.js
@@ -13,3 +13,16 @@ let items = [1,2,3,4,5];
 for(let item of items){
     console.log(item);
 }
+
+// Set (it is iterable)
+//only unique value stored
+
+const myArr = [1,2,2,3,4,4,2,22,3];
+
+const numbers = new  Set(myArr);
+
+console.log(numbers);
+
+for(let num of numbers){
+    console.log(num);
+}
