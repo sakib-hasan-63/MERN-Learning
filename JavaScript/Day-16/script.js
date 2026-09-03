cfe1089986380e379e16dc423cfa30b6b4b3f068
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-09-03 23:26:06 +0530

    Iterable Completed

---
+++ JavaScript/Day-16/script.js
@@ -0,0 +1,15 @@
+// Iterable
+//where we use to for of loop
+//like String and Array
+
+let name = "sakib";
+
+for(let n of name){
+    console.log(n);
+}
+
+let items = [1,2,3,4,5];
+
+for(let item of items){
+    console.log(item);
+}
